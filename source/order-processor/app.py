13e5f8e3f7114c424241a66e69411b806b8f67a2
zero <squiredd@gmail.com>
2023-10-11 20:40:05 -0700

    add testrange

--- source/order-processor/app.py
+++ source/order-processor/app.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 
 app_port = os.getenv('APP_PORT', '6002')
 
-""""
 # Register Dapr pub/sub subscriptions
 @app.route('/dapr/subscribe', methods=['GET'])
 def subscribe():
@@ -22,16 +21,28 @@ def subscribe():
         'pubsubname': 'orderpubsub',
         'topic': 'orders',
         'route': 'orders'
+    },
+    {
+        'pubsubname': 'orderpubsub',
+        'topic': 'testRange',
+        'route': 'orders'
     }]
     print('Dapr pub/sub is subscribed to: ' + json.dumps(subscriptions))
     return jsonify(subscriptions)
-"""
+
 
 # Dapr subscription in /dapr/subscribe sets up this route
 @app.route('/orders', methods=['POST'])
 def orders_subscriber():
     event = from_http(request.headers, request.get_data())
     print('Subscriber received : %s' % event.data['orderId'], flush=True)
+    return json.dumps({'success': True}), 200, {
+        'ContentType': 'application/json'}
+
+@app.route('/testRange', methods=['POST'])
+def orders_subscriber():
+    event = from_http(request.headers, request.get_data())
+    print('testRange received', flush=True)
     return json.dumps({'success': True}), 200, {
         'ContentType': 'application/json'}
 
