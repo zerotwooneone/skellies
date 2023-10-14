63bb525035fae01df9b5de4d77b274c8cc7bd925
zero <squiredd@gmail.com>
2023-10-13 20:04:18 -0700

    add servo to invoke

--- source/invoke-simple/invoke-receiver.py
+++ source/invoke-simple/invoke-receiver.py
@@ -1,4 +1,7 @@
 from dapr.ext.grpc import App, InvokeMethodRequest, InvokeMethodResponse
+import maestro
+import time
+import logging
 
 app = App()
 
@@ -6,6 +9,28 @@ app = App()
 def mymethod(request: InvokeMethodRequest) -> InvokeMethodResponse:
     print(request.metadata, flush=True)
     print(request.text(), flush=True)
+
+    logging.info('received testRange')
+    servo =  maestro.Controller() #/dev/ttyACM1 or ttyACM0(default)
+
+    channels = [0] #range(18)
+    targets = [3000, 3500, 6500, 8000,9000,0] #[1,2000,3000,4000,6000,8000,9000]
+    #speeds = [0,1,60]
+    accels = [1] #,100,255
+    try:
+        #for speed in speeds:
+        for acc in accels:
+            for target in targets:
+                for channelIndex in channels:
+                    logging.info(f'ch: {channelIndex} acc:{acc} tar:{target}')
+                    logging.info(f'min:{servo.getMin(channelIndex)} max:{servo.getMax(channelIndex)} pos:{servo.getPosition(channelIndex)} isMov:{servo.isMoving(channelIndex)} gMov:{servo.getMovingState()}')
+                    #servo.setSpeed(channelIndex,speed)
+                    servo.setAccel(channelIndex,acc)
+                    servo.setTarget(channelIndex,target)
+                    time.sleep(5)
+    finally:
+        logging.info('closing connection')
+        servo.close()
 
     return InvokeMethodResponse(b'INVOKE_RECEIVED', "text/plain; charset=UTF-8")
 
