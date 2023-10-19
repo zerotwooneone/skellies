064ce1d0a2bb7f00d7cb67e95651aee62e97a722
zero <squiredd@gmail.com>
2023-10-18 19:34:15 -0700

    add cig

--- source/invoke-simple/run.py
+++ source/invoke-simple/run.py
@@ -5,26 +5,40 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 commandDelay = 2 #seconds between commands
-channelIndex = 2    
-accel = 5
+cigChannel = 1
+armChannel = 0
+armAccel = 5
+cigAccel = 1
 speed = None
 
 servo =  maestro.Controller() #/dev/ttyACM1 or ttyACM0(default)
 
 try:
     
-    logging.info(f'starting channelIndex: {channelIndex} accel:{accel} speed:{speed}')
+    logging.info(f'starting channelIndex: {channelIndex} accel:{armAccel} speed:{speed}')
     
-    servo.setAccel(channelIndex, accel)
-    time.sleep(1)
-    
+    #setup arm
+    servo.setAccel(armChannel, armAccel)
+    time.sleep(1)    
     #servo.setSpeed(channelIndex, speed)    
     
-    servo.setTarget(channelIndex,3000)
-
+    #move arm
+    servo.setTarget(armChannel,3000)
     time.sleep(commandDelay)
 
-    servo.setTarget(channelIndex,9000)
+    #cig time
+    servo.setAccel(cigChannel, cigAccel)
+    time.sleep(1)
+
+    servo.setTarget(cigChannel,12000)
+    time.sleep(commandDelay)
+
+    servo.setTarget(cigChannel,0)
+    time.sleep(commandDelay)
+
+    #move arm back    
+    servo.setTarget(armChannel,9000)
+    
 finally:
     logging.info('closing connection')
     servo.close()
